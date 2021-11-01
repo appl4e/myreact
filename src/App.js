04d6a92cf1038c5a017b914a4b6d3f03f5876de3
Apple <apple@redlimesolutions.com>
2021-11-01 20:32:07 +0600

    reducing re render of context api

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import Section from './components/Section';
 import ThemeContext from './context/themeContext';
 
 export default class App extends React.Component {
-  state = { theme: 'dark' };
+  state = {
+    theme: 'dark',
+    switchTheme: () => {
+      this.setState(({ theme }) => {
+        if (theme === 'dark') {
+          return { theme: 'light' };
+        }
 
-  switchTheme = () => {
-    this.setState(({ theme }) => {
-      if (theme === 'dark') {
-        return { theme: 'light' };
-      }
-
-      return { theme: 'dark' };
-    });
+        return { theme: 'dark' };
+      });
+    },
   };
 
   render() {
-    const { theme } = this.state;
     return (
       <div className="App">
         <Counter
@@ -26,7 +26,7 @@ export default class App extends React.Component {
             <ClickCounter count={count} incrementCount={incrementCount} />
           )}
         />
-        <ThemeContext.Provider value={{ theme, switchTheme: this.switchTheme }}>
+        <ThemeContext.Provider value={this.state}>
           <Section />
         </ThemeContext.Provider>
       </div>
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,30 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ThemeContext from '../context/themeContext';
 import Counter from './Counter';
 import HoverCounter from './HoverCounter';
 
-export default class Content extends React.Component {
-  componentDidMount() {
-    console.log(this.context);
-  }
-
-  render() {
-    const { theme, switchTheme } = this.context;
-    return (
-      <div>
-        <h1>This is a content</h1>
-        <Counter
-          render={(count, incrementCount) => (
-            <HoverCounter
-              count={count}
-              incrementCount={incrementCount}
-              theme={theme}
-              switchTheme={switchTheme}
-            />
-          )}
-        />
-      </div>
-    );
-  }
+export default function Content() {
+  const { theme, switchTheme } = useContext(ThemeContext);
+  return (
+    <div>
+      <h1>This is a content</h1>
+      <Counter
+        render={(count, incrementCount) => (
+          <HoverCounter
+            count={count}
+            incrementCount={incrementCount}
+            theme={theme}
+            switchTheme={switchTheme}
+          />
+        )}
+      />
+    </div>
+  );
 }
-Content.contextType = ThemeContext;
