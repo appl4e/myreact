3914f9771c362635f29c3d71d0a6f164e270d5c5
Apple <apple@redlimesolutions.com>
2024-04-01 23:30:18 +0600

    urlSearchParam, for loop, pagination

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { ReactElement, useEffect, useState } from "react";
 import PageCover from "../components/PageCover";
 import Post from "../components/Post";
 import { PostContent } from "./../interfaces/post";
@@ -6,20 +6,38 @@ import { PostContent } from "./../interfaces/post";
 const Home = () => {
 	const [isLoading, setIsLoading] = useState(true);
 	const [posts, setPosts] = useState<PostContent[]>([]);
+	const [userId, setUserId] = useState<string>("1");
 	useEffect(() => {
-		fetch("https://jsonplaceholder.typicode.com/posts?userId=1")
+		setIsLoading(true);
+		fetch("https://jsonplaceholder.typicode.com/posts?" + new URLSearchParams({ userId: userId }))
 			.then((res) => res.json())
 			.then((data) => {
 				setPosts(data);
 				setIsLoading(false);
 			});
-	}, []);
+	}, [userId]);
+	const selects: ReactElement[] = [];
+	for (let i = 1; i < 11; i++) {
+		selects.push(
+			<option key={i} value={i}>
+				{i}
+			</option>
+		);
+	}
 	return (
 		<>
 			<PageCover title="This is our Home Page"></PageCover>
 
 			<div className="mx-4 py-4">
-				<h2 className="font-bold text-3xl mb-4">Posts</h2>
+				<div className="flex  w-8/12">
+					<h2 className="font-bold text-3xl mb-4">Posts</h2>
+					<div className="flex ms-auto items-center gap-2">
+						<label>Page</label>
+						<select className="border border-slate-700 block rounded-sm" onChange={(e) => setUserId(e.target.value)}>
+							{selects}
+						</select>
+					</div>
+				</div>
 
 				<div className=" w-8/12">
 					{isLoading ? <div className="text-gray-500">Loading ...</div> : posts.map((post) => <Post content={post} key={post.id} />)}
